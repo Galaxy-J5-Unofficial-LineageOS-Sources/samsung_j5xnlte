d099f861df36ea8265892a35f94da021ddac51ba
David <daviiideveloper@gmail.com>
2022-03-21 13:01:56 +0100

    j5xnlte - Missing repo

--- device.mk
+++ device.mk
@@ -1,11 +1,14 @@
 #Inherit from vendor
 $(call inherit-product-if-exists, vendor/samsung/j5xnlte/j5xnlte-vendor.mk)
 
-# Inherit J5 2015 device
+# Inherit J5 2016 device
 $(call inherit-product, device/samsung/msm8916-common/device_j5xnlte.mk)
 
 # Inherit from common
 $(call inherit-product, device/samsung/msm8916-common/lineage_msm8916.mk)
+
+# Inherit from common
+$(call inherit-product, device/samsung/j5x-common/device-common.mk)
 
 LOCAL_PATH := device/samsung/j5xnlte
 
