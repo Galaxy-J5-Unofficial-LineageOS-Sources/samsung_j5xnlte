44d877c7e5a2d34c0aa47a3068a97acefd1ec5e4
David <daviiideveloper@gmail.com>
2022-03-22 04:14:13 +0100

    j5xnlte: Match J5 2015 makefile

--- lineage_j5xnlte.mk
+++ lineage_j5xnlte.mk
@@ -1,5 +1,8 @@
 $(call inherit-product, device/samsung/j5xnlte/device.mk)
 
+# Inherit from common
+$(call inherit-product, device/samsung/j5x-common/lineage.mk)
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := j5xnlte
 PRODUCT_NAME := lineage_j5xnlte
